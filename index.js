d114904dc36c15fe2e8394c56919ca89d9ae5c66
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-09-07 16:14:42 +0300

    coding about the js section

---
+++ index.js
@@ -0,0 +1,28 @@
+const testimonials = [
+    {
+        name: "AI generator",
+        photoUrl: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDd8fHVzZXJ8ZW58MHx8MHx8fDA%3D",
+        text: "I've been an Apple user for over a decade, everything just works seamlessly together. Apple truly knows how to make technology intuitive and user-friendly.",
+        name: "Open AI",
+        photoUrl: "https://images.unsplash.com/photo-1685903772095-f07172808761?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDN8fHVzZXJ8ZW58MHwxfDB8fHwy",
+        text: "It seems like you're referring to apple.Could you clarify whether you're asking about the fruit, the technology company (Apple Inc.), or something else related to apples?",
+    }
+];
+
+const imgEl = document.querySelector("img");
+const textEl = document.querySelector(".text");
+const usernameEl = document.querySelector(".username");
+
+let idx = 0;
+
+updateTestimonial();
+
+function updateTestimonial(){
+    const {name,photoUrl,text} = testimonials[idx];
+    imgEl.src = photoUrl;
+    textEl.innerText = name;
+    idx++;
+    setTimeout(()=>{
+        updateTestimonial();
+    },2000)
+}
